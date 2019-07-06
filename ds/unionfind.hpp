a15bdba6d4c0004f0f350691b6a0e3667a9d6c76
flyio <singularpoint.org@gmail.com>
2019-07-06 20:33:07 +0900

    PotentialUnionFind implemented

--- ds/unionfind.hpp
+++ ds/unionfind.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <utility>
 
 struct UnionFind {
   int n;
@@ -18,4 +19,35 @@ struct UnionFind {
       if(rank[x]==rank[y]) rank[x]++;
     }
   }
+};
+
+// ポテンシャル Union-Find木
+// * 通常のUF木の操作に加えて重み (ポテンシャル) を管理する機能を追加
+// * root, same は同じ
+// * unite(x,y,w) は x, y を unite するが，その際に weight(y)-weight(x) が w になるように重みを設定する
+// * diff(x,y) は weight(y) - weight(x) を返す
+// * 詳細は https://qiita.com/drken/items/cce6fc5c579051e64fab
+// * See Also: (AOJ) http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_B
+struct PotentialUnionFind {
+  int n;
+  std::vector<int> par;
+  std::vector<int> rank;
+private:
+  std::vector<int> diff_w; // diff_w[v] means the difference of weight from its parent
+public:
+  PotentialUnionFind(int n_) : par(n_), rank(n_,0), diff_w(n_,0), n(n_) { for(int i=0;i<n;++i) par[i] = i; }
+  int root(int x) { int r; return par[x] == x ? x : (r=root(par[x]), diff_w[x]+=diff_w[par[x]], par[x]=r); }
+  bool same(int x, int y) { return root(x)==root(y); }
+  bool unite(int x, int y, int w) {
+    w += weight(x); w-=weight(y);
+    x = root(x); y = root(y);
+    if(x==y) return false;
+    if(rank[x] < rank[y]) std::swap(x,y), w=-w;
+    if(rank[x]==rank[y]) rank[x]++;
+    par[y] = x; diff_w[y] = w;
+    return true;
+  }
+  int diff(int x, int y){ return weight(y)-weight(x); }
+private:
+  int weight(int x){ root(x); return diff_w[x]; }
 };
